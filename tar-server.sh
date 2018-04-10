6197b549003a5d6938c34c645d81d948d6cd5b67
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 17:29:55 -0500

    Update tar-server.sh

--- tar-server.sh
+++ tar-server.sh
@@ -3,7 +3,7 @@ LIST=`mktemp`
 #
 find /storage/Misc/backup/"$servername" -type s -print >> $LIST
 #
-env GZIP=-9 tar -czf '/storage/Misc/backup/archived/"$servername"$(date '+%d%m%y%').tar.gz' -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/"$servername/"
+env GZIP=-9 tar -czf '/storage/Misc/backup/archived/"$servername"$(date '+%d%m%y%').tar.gz' -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/"$servername"/
 #
 rm $LIST
 #
