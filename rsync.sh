5be5179ec174d4a6ac052f9e19b9884f0944feac
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 16:16:33 -0500

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -lHhtrvz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --exclude=/lost+found/ --no-specials --chmod=ugo=rwX root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
+rsync -lHEhtrvz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --exclude=/lost+found/ --no-specials --chmod=ugo=rwX root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
