bde5ecb1bd119ac6905f86025efdc143792ac20c
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 16:37:25 -0500

    Update tar-server.sh

--- tar-server.sh
+++ tar-server.sh
@@ -1,4 +1,14 @@
-
+#!/bin/sh
+LIST=`mktemp`
+#
+find /storage/Misc/backup/"$servername" -type s -print >> $LIST
+#
+env GZIP=-9 tar -czf "/storage/Misc/backup/archived/"$servername"$(date '+%d%m%y%').tar.gz" -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/"$servername/
+#
+rm $LIST
+#
+rm -r /storage/Misc/backup/"$servername"
+#
 echo "Server $servername Backup archived successfully"
 read servername
 export servername
