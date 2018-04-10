127a1b41dfcef8b987f13b0a947f4c5c09d9e27e
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 16:28:17 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,1 +1,5 @@
-ssh -t root@server210-3.jeffwcollins.com "apt-get update; apt-get upgrade -y; apt-get autoremove; apt-get clean; apt-get autoclean"
+ssh -t root@"$servername".jeffwcollins.com "apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get clean && apt-get autoclean"
+echo "Server $servername cleaned completed Successfully"
+read servername
+export servername
+./rsync-server.sh
