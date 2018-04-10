cd8fca34810833055c95ea1afd644a05c25b2e3d
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 11:56:13 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,5 +1,6 @@
 ssh -t root@server210-3.jeffwcollins.com "apt-get update && apt-get upgrade -y"
-
+#
 ssh -t root@server210-3.jeffwcollins.com "apt-get autoremove"
-
+#
 ssh -t root@server210-3.jeffwcollins.com bash "apt-get autoclean"
+#
