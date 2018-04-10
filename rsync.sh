1e39903f71ecf306e1c82c1f2bf8b460827b137c
jeffwcollins <jeffwcollins@gmail.com>
2018-04-10 14:33:40 -0500

    formating

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -avxz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --no-specials root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
+rsync -lhtrvxz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --no-specials root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
--- tar.sh
+++ tar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 LIST=`mktemp`
 find /storage/Misc/backup/server210-3vm -type s -print >> $LIST
-env GZIP=-9 tar -czf /storage/Misc/backup/archived/server210-3vm.tar.gz -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/server210-3vm
+env GZIP=-9 tar -czf /storage/Misc/backup/archived/server210-3vm.tar.gz -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/server210-3vm/
 rm $LIST
