3b24341df2103efd4dbb310d82668f064321ad48
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 13:27:25 -0500

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
+rsync -avxz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --no-specials root@server210-3.jeffwcollins.com:/ /storage/Misc/backup/server210-3vm
