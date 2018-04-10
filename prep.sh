8fdc48067ab73778b91ce22b4599397914ee93dc
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 12:14:03 -0500

    typo

--- prep.sh
+++ prep.sh
@@ -1,1 +1,1 @@
-ssh -t root@server210-3.jeffwcollins.com "apt-get update; apt-get ugrade -y; apt-get autoremove; apt-get clean; apt-get autoclean"
+ssh -t root@server210-3.jeffwcollins.com "apt-get update; apt-get upgrade -y; apt-get autoremove; apt-get clean; apt-get autoclean"
