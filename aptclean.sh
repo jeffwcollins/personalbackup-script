8b90e3959d8788de47b7cf34f78dd0f603737abf
jeffwcollins <jeffwcollins@gmail.com>
2018-04-10 11:40:24 -0500

    try again

--- aptclean.sh
+++ aptclean.sh
@@ -1,2 +1,1 @@
-ssh -t root@server210-3.jeffwcollins.com 'apt-get update && apt-get -y upgrade'
-ssh -t root@server210-3.jeffwcollins.com 'sudo apt-get clean && sudo apt-get autoclean'
+apt-get autoremove
