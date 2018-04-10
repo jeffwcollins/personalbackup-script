abd169e1faf7a53fc10a51d3f2dae1b53c514bbb
jeffwcollins <jeffwcollins@gmail.com>
2018-04-10 12:39:16 -0500

    update tar

--- tar.sh
+++ tar.sh
@@ -1,1 +1,1 @@
-env GZIP=-9 tar --exclude='server210-3vm/tmp' --exclude='server210-3vm/sys' --exclude='server210-3vm/proc' -cvzf server210-3vm.tar.gz server210-3vm
+env GZIP=-9 tar -cvzf server210-3vm.tar.gz server210-3vm
