12b6d681f2d2cc3970eb321c5d5653c6859e40dd
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 10:29:42 -0500

    Update aptclean.sh

--- aptclean.sh
+++ aptclean.sh
@@ -1,5 +1,5 @@
 ssh -t root@server210-3.jeffwcollins.com 'apt-get update'
-sleep 15
+sleep 15s
 ssh -t root@server210-3.jeffwcollins.com 'apt-get upgrade -y'
-sleep 60
+sleep 60s
 ssh -t root@server210-3.jeffwcollins.com 'apt-get clean'
