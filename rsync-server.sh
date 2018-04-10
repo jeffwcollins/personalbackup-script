f861c0ff50c368e2570dd90de65f9c0523b22ebc
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 16:43:41 -0500

    Update rsync-server.sh

--- rsync-server.sh
+++ rsync-server.sh
@@ -1,5 +1,4 @@
 rsync -lHEhtrvz --exclude=/dev/ --exclude=/proc/ --exclude=/tmp/ --exclude=/sys/ --exclude=/lost+found/ --no-specials --chmod=ugo=rwX root@"$servername".jeffwcollins.com:/ /storage/Misc/backup/"$servername"
 echo "Server $servername RSYNC completed successfully"
-read servername
 export servername
 ./tar-server.sh
