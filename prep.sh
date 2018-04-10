26387766ec40c8abf3f252280bd161912f688998
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 11:50:19 -0500

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,16 +1,16 @@
 ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF
-'apt-get update'
+apt-get update
 EOF
 '"
-ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF
-'apt-get upgrade -y'
-EOF
+ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF1
+apt-get upgrade -y
+EOF1
 '"
-ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF
-'apt-get autoremove'
-EOF
+ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF2
+apt-get autoremove
+EOF2
 '"
-ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF
-'apt-get autoclean'
-EOF
+ssh -t root@server210-3.jeffwcollins.com bash -c "' << EOF3
+apt-get autoclean
+EOF3
 '"
