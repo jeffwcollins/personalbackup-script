837a1593221b1446e1d3a73c5aa4f97bdf0abc31
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-04-10 13:16:47 -0500

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 LIST=`mktemp`
-find /storage/Misc/backup/server210-3vm -type s -print > $LIST
-env GZIP=-9 tar -czf /storage/Misc/backup/archived/server210-3vm.tar.gz -X $LIST \
---exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt \
-/storage/Misc/backup/server210-3vm
+find /storage/Misc/backup/server210-3vm -type s -print >> $LIST
+env GZIP=-9 tar -czf /storage/Misc/backup/archived/server210-3vm.tar.gz -X $LIST --exclude=/proc --exclude=/lost+found --exclude=/tmp --exclude=/sys --exclude=/mnt /storage/Misc/backup/server210-3vm
 rm $LIST
